59940b8950e12eda7f8c41b7bd5d999a5ebf68f9
PLJC-PC <taufikqurrahman330@gmail.com>
2019-10-11 21:26:56 +0700

    Initial commit

---
+++ app/src/main/java/ibrahim999/lp3i/Phoenix/MainActivity.java
@@ -0,0 +1,14 @@
+package ibrahim999.lp3i.Phoenix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
